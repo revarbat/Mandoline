73a1bd92aa8a4eda7eb55d1030f70c2512d48ad4
Matt Mets <matt.mets@cibomahto.com>
2011-05-20 11:04:30 -0700

    Hack on a gcode exporter.

--- src/GCodeExportOp.cc
+++ src/GCodeExportOp.cc
@@ -9,7 +9,8 @@
 #include "GCodeExportOp.h"
 #include "BGL/BGL.h"
 
-
+#include <iostream>
+#include <fstream>
 
 GCodeExportOp::GCodeExportOp(SlicingContext* ctx)
     : Operation(), context(ctx)
@@ -23,6 +24,59 @@ GCodeExportOp::~GCodeExportOp()
 }
 
 
+ostream &pathToGcode(Path& path, float zHeight, ostream& out) {
+    Lines::iterator line = path.segments.begin();
+
+    out << "M103" << endl;
+
+    // Move to the starting position...
+    out << "G1 X" << line->startPt.x
+        << " Y" << line->startPt.y
+        << " Z" << zHeight
+        << std::endl;
+    out << "M101" << endl;
+
+    for (;
+         line != path.segments.end();
+         line++) {
+
+
+        out << "G1 X" << line->endPt.x
+            << " Y" << line->endPt.y
+            << " Z" << zHeight
+            << std::endl;
+    }
+
+    return out;
+}
+
+ostream &pathsToGcode(Paths& paths, float zHeight, ostream& out) {
+    // TODO: inner paths?
+    Paths::iterator pit;
+    for (pit = paths.begin();
+         pit != paths.end();
+         pit++) {
+        pathToGcode((*pit), zHeight, out);
+    }
+
+    return out;
+}
+
+
+ostream &simpleRegionsToGcode(SimpleRegions& regions, float zHeight, ostream& out) {
+    SimpleRegions::iterator perimeter;
+    for (perimeter = regions.begin();
+         perimeter != regions.end();
+         perimeter++) {
+
+        pathToGcode(perimeter->outerPath, zHeight, out);
+
+        // TODO: inner paths?
+        pathsToGcode(perimeter->subpaths, zHeight, out);
+    }
+
+    return out;
+}
 
 void GCodeExportOp::main()
 {
@@ -30,6 +84,37 @@ void GCodeExportOp::main()
     if ( NULL == context ) return;
 
     // TODO: join paths and optimize them
+    std::cout << "Gcode pather starting" << std::endl;
+    // For each slice, write out code.
+    std::cout << "Layer count: " << context->slices.size() << std::endl;
+
+    map<float,CarvedSlice>::iterator it;
+
+    fstream fout;
+    fout.open("out.gcode", fstream::out | fstream::trunc);
+    if (!fout.good()) {
+        return;
+    }
+
+    fout << "M108 R35" << std::endl;
+    fout << "M108 R2" << std::endl;
+
+    // For each layer....
+    for (it = context->slices.begin(); it != context->slices.end(); it++) {
+        fout << "(new layer)" << std::endl;
+
+        CarvedSlice* slice = &(*it).second;
+
+        fout << "(perimeter)" << std::endl;
+
+        simpleRegionsToGcode(slice->perimeter.subregions, (*it).first, fout);
+
+        fout << "(infill)" << std::endl;
+
+        pathsToGcode(slice->infill, (*it).first, fout);
+    }
+
+    fout.close();
 
     if ( isCancelled ) return;
 }
